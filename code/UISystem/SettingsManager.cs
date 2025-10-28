370a41f0657aea3c5e35488b8522333a190c280a
Paavo Westerlund <paawes@utu.fi>
2025-10-28 23:54:31 +0200

    SettingsManager tracks if changes were made before save
    Settings menu now saves even if goback is not called

--- code/UISystem/SettingsManager.cs
+++ code/UISystem/SettingsManager.cs
@@ -7,6 +7,8 @@ public class SettingsManager : SingletonBase<SettingsManager>
 	public PlayerPreferences PlayerPreferences => playerPreferences;
 
 	public bool IsLoaded { get; private set; } = false;
+	// !!NOTE: Remember to add to all new setting setters or come up with new pattern
+	private bool stateChanged = false; // To avoid unnecessary writes
 
 	public event Action<CrosshairType> OnCrosshairStyleChanged;
     public event Action<string> OnCrosshairColorChanged;
@@ -24,7 +26,9 @@ public class SettingsManager : SingletonBase<SettingsManager>
 
 		playerPreferences.CrosshairStyle = type;
         OnCrosshairStyleChanged?.Invoke(type);
-    }
+
+		stateChanged = true;
+	}
 
     public void SetCrosshairColor(string hex)
     {
@@ -32,11 +36,16 @@ public class SettingsManager : SingletonBase<SettingsManager>
 
 		playerPreferences.CrosshairColor = hex;
         OnCrosshairColorChanged?.Invoke(hex);
-    }
+
+		stateChanged = true;
+	}
 
     public void Save() // Called in SettingsMenu hide
     {
+		if ( !IsLoaded || !stateChanged ) return;
+
 		FileManager.Save<PlayerPreferences>( playerPreferences );
+		stateChanged = false;
     }
 
     public void Load()
