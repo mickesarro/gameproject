11dc85686c4771436c47d199f6a6ff106b4f62aa
Markus Leppihalme <markus.m.leppihalme@utu.fi>
2025-12-04 20:40:39 +0000

    network fixes

--- code/Player/PlayerStats.cs
+++ code/Player/PlayerStats.cs
@@ -4,28 +4,37 @@ namespace Shooter;
 
 public sealed class PlayerStats : Component, ISerializable
 {
-	public int Score { get; private set; } = 0;
-	public int Kills { get; private set; } = 0;
-	public float Damage { get; private set; } = 0;
-	public int Deaths { get; private set; } = 0;
+    [Sync] public int Score { get; private set; } = 0;
+    [Sync] public int Kills { get; private set; } = 0;
+    [Sync] public float Damage { get; private set; } = 0;
+    [Sync] public int Deaths { get; private set; } = 0;
+    
+    public string Name => "player_stats";
+    public bool ShouldAccumulate => true;
 
-	public string Name => "player_stats";
-	public bool ShouldAccumulate => true;
+    [Rpc.Owner]
+    public void AddKill()
+    {
+        Log.Info("AddKill");
+        Kills++;
+    }
+    public void AddDeath()
+    {
+        Log.Info("AddDeath");
+        Deaths++;
+    }
+    [Rpc.Owner]
+    public void AddDamage(float damage) => Damage += damage;
+    [Rpc.Owner]
+    public void AddScore(int amount)   => Score += amount;
+    
+    public void Accumulate( ISerializable data )
+    {
+        if ( data == null || data is not PlayerStats stats ) return;
 
-	// Assists, damage, favourite gun etc.
-
-	public void AddKill() => Kills++;
-	public void AddDamage( float damage ) => Damage += damage; 
-	public void AddDeath() => Deaths++;
-	public void AddScore( int amount ) => Score += amount;
-
-	public void Accumulate( ISerializable data )
-	{
-		if ( data == null || data is not PlayerStats stats ) return;
-
-		this.Score += stats.Score;
-		this.Kills += stats.Kills;
-		this.Damage += stats.Damage;
-		this.Deaths += stats.Deaths;
-	}
+        this.Score += stats.Score;
+        this.Kills += stats.Kills;
+        this.Damage += stats.Damage;
+        this.Deaths += stats.Deaths;
+    }
 }
