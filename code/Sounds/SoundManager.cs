089240c77bfdd937291a5beae0684bc8e6fa73af
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2025-12-10 19:13:02 +0200

    Added rocket launch sound

--- code/Sounds/SoundManager.cs
+++ code/Sounds/SoundManager.cs
@@ -25,7 +25,7 @@ public static class SoundManager
         {SoundType.GunshotAR, "sounds/weapons/m4a1 shot.sound" },
         {SoundType.Explosion, "sounds/weapons/explosion_urban.sound"},
         {SoundType.Hitmarker, "sounds/weapons/hitmarker.sound"},
-        {SoundType.GunshotRocket, "sounds/weapons/rocketlauncherlaunchbit.sound"},
+        {SoundType.GunshotRocket, "sounds/weapons/rocket_launch.sound"},
         {SoundType.Reload, "sounds/weapons/m4 reload sequence.sound"},
         {SoundType.OutOfAmmo, "sounds/weapons/rifle-clip-empty.sound"},
         //{SoundType.GunshotRocket, "sounds/weapons/rocket_launcher/rocketlauncherlaunchbit.sound_c"},
