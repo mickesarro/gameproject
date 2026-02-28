5a004e5c1f9dbed3b70413caeb569ff1fbe225fd
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2026-03-01 00:30:52 +0200

    Fixed jumppad velocity add and added flag for trampoline

--- code/LevelElements/JumpPad.cs
+++ code/LevelElements/JumpPad.cs
@@ -9,16 +9,41 @@ namespace Shooter;
 public sealed class JumpPad : Component, Component.ITriggerListener
 {
 	[Property] private float Force { get; set; } = 100f;
+    private Vector3 JumpForce = Vector3.Zero;
 
-	public void OnTriggerEnter( Collider other )
+    [Property, ToggleGroup( "Trampoline", Label = "Trampoline" )]
+    private bool Trampoline { get; set; } = false;
+    [Property, ToggleGroup( "Trampoline" )] private float Factor { get; set; } = 1.5f;
+
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+
+        JumpForce = WorldTransform.Up * Force;
+    }
+
+
+    public void OnTriggerEnter( Collider other )
 	{
 		if ( other.IsProxy ) return;
 
-		other.GameObject.Root.GetComponent<ICharacterBase>()
-			?.ApplyForce( WorldTransform.Up * Force );
+        if ( !other.GameObject.Root.Components.TryGet<ICharacterBase>( out var character ) ) return;
 
-		// Makes sure that bots hitting the JumpPad don't play the sound to you locally
-		if ( other.GameObject.Root.Components.TryGet<PlayerController>( out _ ) )
+        var forceToApply = JumpForce;
+
+        float velToPad = WorldTransform.Up.Dot( character.Velocity );
+        // Only cancel motion if moving downwards
+        if ( velToPad < 0 )
+        {
+            forceToApply += !Trampoline
+                ? -WorldTransform.Up * velToPad
+                : WorldTransform.Up * (-velToPad * Factor);
+        }
+
+        character.ApplyForce( forceToApply );
+
+        // Makes sure that bots hitting the JumpPad don't play the sound to you locally
+        if ( character.IsPlayer )
 		{
 			SoundManager.PlayLocal( SoundManager.SoundType.JumpPad, 0.5f );
 		}
