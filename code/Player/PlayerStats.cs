b96de298847ee612486f4e06138507ce7b0b24d0
Paavo Westerlund <paawes@utu.fi>
2026-03-08 18:19:27 +0000

    Stats now sync to cloud, added win to stats.

--- code/Player/PlayerStats.cs
+++ code/Player/PlayerStats.cs
@@ -8,6 +8,7 @@ public sealed class PlayerStats : Component, ISerializable
     [Sync] public int Kills { get; private set; } = 0;
     [Sync] public float Damage { get; private set; } = 0;
     [Sync] public int Deaths { get; private set; } = 0;
+    [Sync] public int Wins { get; private set; } = 0;
     
     public string Name => "player_stats";
     public bool ShouldAccumulate => true;
@@ -31,6 +32,9 @@ public sealed class PlayerStats : Component, ISerializable
     [Rpc.Owner]
     public void AddScore(int amount)   => Score += amount;
 
+    [Rpc.Owner]
+    public void IncrementWins() => Wins++;
+
     public void Accumulate( ISerializable data )
     {
         if ( data == null || data is not PlayerStats stats ) return;
@@ -39,5 +43,16 @@ public sealed class PlayerStats : Component, ISerializable
         this.Kills += stats.Kills;
         this.Damage += stats.Damage;
         this.Deaths += stats.Deaths;
+    }
+
+    public void UpdateRemoteStats()
+    {
+        if ( GameObject.Tags.Has( "npc" ) || Application.IsEditor ) return;
+
+        Sandbox.Services.Stats.Increment( "Score", Score );
+        Sandbox.Services.Stats.Increment( "Kills", Kills );
+        Sandbox.Services.Stats.Increment( "Deaths", Deaths );
+        Sandbox.Services.Stats.Increment( "Damage", Damage );
+        Sandbox.Services.Stats.Increment( "Wins", Wins );
     }
 }
